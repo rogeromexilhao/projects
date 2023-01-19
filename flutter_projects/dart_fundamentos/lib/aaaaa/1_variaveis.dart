ec267cc436f9948b055489927541ee03b29b938d
rodrigodesktop <rodrigo.lago.sardin170401@gmail.com>
2023-01-19 17:51:06 -0300

    initial commit

---
+++ flutter_projects/dart_fundamentos/lib/aaaaa/1_variaveis.dart
@@ -0,0 +1,27 @@
+void main(){
+  //int 
+  int idade = 21;
+
+  //float
+  double valor = 10.000;
+
+  //str
+  String str = 'teste';
+
+  //num pode ser qualquer numero
+  num numero = 1;
+  num numero2 = 1.5;
+
+  //pode usar var direto
+  var teste = 10;
+  var teste2 = 'alo alo';
+
+  //object pode ser td onde tds as outra variaveis são filhos de object
+  /*
+  object geralemnte usa para alguma coisa que vc n sabe o que vai receber
+  */
+  Object teste3 = 'qualquer coisa';
+
+  //dynamic funciona da msm maneira que o object
+  dynamic teste4 = 12312312;
+}
