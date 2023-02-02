86cab8686d79374f3ec396766e875c5364a16b78
Rodrigonot <rodrigo.lago.sardin170401@gmail.com>
2023-02-02 10:50:12 -0300

    commit 02/02

--- flutter_projects/dart_fundamentos/lib/aaaaa/1_variaveis.dart
+++ flutter_projects/dart_fundamentos/lib/aaaaa/1_variaveis.dart
@@ -32,4 +32,41 @@ void main(){
   String? nome = null;
   //o '?' serve como ondicional pode ser null mas pode ser outro valor
   print(nome);
+
+  //o final faz com que o valor que esta setado n podera ser mais mudado no caso abaixo vai ser semre Rodrigo lago 
+  //e nao podera ser alterado futuramente vai torna la imutavel 
+  final nome2 = 'Rodrigo lago';
+  print(nome2);
+
+  //o const tb torna a variavel imutavel porem ela tem uma particulariedade que é a variavel deve ser reconhecida antes da compilação
+  const nomeconst = 'teste de nome';
+  //se fizermos o caso abaixo temos um erro
+  //const teste = numero;
+
+  //if else else if suas declarações são iguais ao javascript
+  if (idade >= 18){
+    print('mior de 18');
+  }else{
+    print('menor de 18');
+  }
+
+  //codição ternario (in line) igual do react
+  final miordeidade = (idade >= 18 ? true : false);
+  print(miordeidade);
+  
+  
+  //switch case
+  var cont = 2;
+  switch(cont){
+    case 0:
+      print('case 0');
+      break;
+    case 1:
+      print('case 1');
+      break;
+    default:
+      print('nada');
+      break;
+  }
+
 }
