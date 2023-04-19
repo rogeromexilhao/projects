9bb8dbc86b42364e22816f3c3a6e8f5a3e007e30
rogeromexilhao <rodrigo.lago.sardin170401@gmail.com>
2023-04-19 19:25:49 -0300

    analisadores TERMINADOS

--- Python/analisador_lexico/teste.py
+++
@@ -1,9 +0,0 @@
-with open("codigo1.txt", "r") as texto:
-    arquivo = texto.readlines()
-    lista = []
-    for linha in arquivo:
-        line = linha.strip("\n") + " "
-        lista.append(line)
-texto.close()
-
-print(lista)
