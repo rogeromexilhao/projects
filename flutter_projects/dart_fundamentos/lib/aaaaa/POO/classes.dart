b25ffa9a95c3c770c7ad9008d7bbaa208e9440a1
rogeromexilhao <rodrigo.lago.sardin170401@gmail.com>
2023-08-14 13:02:59 -0300

    aulas

--- flutter_projects/dart_fundamentos/lib/aaaaa/POO/classes.dart
+++ flutter_projects/dart_fundamentos/lib/aaaaa/POO/classes.dart
@@ -1,16 +1,32 @@
 // declarar uma classe 
 class Camiseta{
-  //atributos
+  //atributos de instancia 
   String? tamanho;
-  String? cor;
+  String? _cor; //esse atributo ficou privado
   String? marca;
 
-  //são chamados de métodos
+  //info que não muda info statica
+  //atributos de classe
+
+  //static String nome = 'camiseta';
+  static const String nome = 'camiseta'; //dessa forma n temos a alteraçãodo valor
+
+  //metodos de classe
+  static String recuperarnome() =>  nome;
+
+  //são chamados de métodos de instancia
   String? lavagem(){
     if (marca == 'nike'){
       return 'nao pode lavar na maquinja';
     }else{
       return 'pode lavar na maquinja';
+    }
+  }
+
+  String? get cor => _cor;
+  set cor(String? cor){
+    if(cor == 'Verde'){
+      throw Exception('não pode ser verder');
     }
   }
 }
--- flutter_projects/dart_fundamentos/lib/aaaaa/POO/main.dart
+++ flutter_projects/dart_fundamentos/lib/aaaaa/POO/main.dart
@@ -3,9 +3,14 @@ import 'classes.dart';
 void main(){
   var camisetanNike = Camiseta();
   camisetanNike.tamanho = 'G';
-  camisetanNike.cor = 'preta';
+  camisetanNike.cor = 'Verde';
   camisetanNike.marca = 'nike';
 
+  print(Camiseta.nome);
+  print(Camiseta.recuperarnome());
+  
+  //Camiseta.nome = 'Camiseta nike'; se fizermos isso a partir desse ponto passamos o nome de 'camiseta' para 'Camiseta Nike' ate o fim da exec
+  
   print(camisetanNike.tamanho);
   print(camisetanNike.cor);
   print(camisetanNike.marca);
