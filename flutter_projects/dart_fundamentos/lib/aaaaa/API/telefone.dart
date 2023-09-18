f3d0fb72d31260e0202f88d9604bc6f0f00be691
rogeromexilhao <rodrigo.lago.sardin170401@gmail.com>
2023-09-18 16:48:10 -0400

    serialização e dessserialização

--- flutter_projects/dart_fundamentos/lib/aaaaa/API/telefone.dart
+++ flutter_projects/dart_fundamentos/lib/aaaaa/API/telefone.dart
@@ -1,6 +1,31 @@
+import 'dart:convert';
+
 class Telefone{
   int ddd;
   String telefone;
 
   Telefone({required this.ddd,required this.telefone});
+  
+  //serialização
+  //passo 1 mapa de chave valor
+  Map<String,dynamic> toMap(){
+    return{'ddd':ddd,'telefone':telefone};
+  }
+
+  //passo 2 
+  String toJson() => jsonEncode(toMap());
+
+
+  factory Telefone.fromMap(Map<String,dynamic> map){
+    return Telefone(
+      ddd:map['ddd'] ?? 0,
+      telefone:map['telefone'] ?? ''
+    );
+  }
+
+
+  factory Telefone.fromJson(String json){
+    final jsonMap = jsonDecode(json);
+    return Telefone.fromMap(jsonMap);
+  }
 }
